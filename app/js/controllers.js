871659e02bbf15b8ac7dd4fe8acd662680e23678
louis hoang <bawsing@gmail.com>
2015-05-23 21:43:31 -0700

    more comment

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,15 +14,21 @@ angular.module('myApp.controllers', [])
     var partiesRef = new Firebase('https://waitandeat-louishoan.firebaseio.com/parties');
 
     // Get data from firebase SERVICE;
+    // This will print out a collection of data from database,
+    // similar to index action on rails
     $scope.parties = $firebase(partiesRef);
 
+    // similar to new action rails where we initialize an instance
     $scope.newParty = {name: '', phone: '', size: '', done: false, notified: "No"};
 
+    //similar to create action in rails
     $scope.saveParty = function(){
       // Add data to fire base using .$add
       $scope.parties.$add($scope.newParty);
       $scope.newParty = {name: '', phone: '', size: '', done: false, notified: "No"};
     };
+
+
 
     // send message start
     $scope.sendTextMessage = function(party){
@@ -35,7 +41,7 @@ angular.module('myApp.controllers', [])
       };
       textMessages.$add(newTextMessage);
 
-      // Notified?
+      // Change notified status
       party.notified = "Yes";
       $scope.parties.$save(party.$id);
     };
